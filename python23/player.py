3341c108e30d157c4b22cd98d0bd7ebd5b940ede
SangKwon93 <apfhda7@gmail.com>
2022-09-27 23:30:20 +0900

    ping_pong_game_project_with_turtle_module

---
+++ python23/player.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+# 거북이가 길을 건너
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
