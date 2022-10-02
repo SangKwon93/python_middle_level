75efa9200f17a94fdf68d4dbd20d5d96663182da
SangKwon93 <apfhda7@gmail.com>
2022-10-02 22:05:39 +0900

    fix : data.txt 파일을 열어서 최고점수를 저장 및 기록하게 코드 수정

--- python20_21/scoreboard.py
+++ python20_21/scoreboard.py
@@ -3,10 +3,16 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ('Arial', 20, 'normal')
 
+
+# 재시작을 하면 최고점수가 리셋되기에 data.txt를 만들어서 최고 점수를 계속 저장해서 추적하게 코드 리밸런싱
+
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
+        with open('data.txt') as data:
+            self.high_score=int(data.read())
+
         self.penup()
         self.goto(0, 260)
         self.color("white")
@@ -15,16 +21,29 @@ class ScoreBoard(Turtle):
 
 
     def update_scoreboard(self):
-        self.write(f"score board: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        # 최고점수도 표시
+        self.write(f"Score board: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=ALIGNMENT , font=FONT)
+    # game_over 메소드 제거
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over", align=ALIGNMENT , font=FONT)
+
+    # 리셋하고 최고점수 data.txt 기록하기
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score=self.score
+            with open("data.txt",mode="w")as data:
+                data.write(f"{self.high_score}")
+            self.score=0
+            self.update_scoreboard()
+
 
     def increase_score(self):
 
         self.score +=1
-        self.clear()
+
         self.update_scoreboard()
 
 
