ca1b4b2031b90fcc43e997e4d7a8b7b304175ad9
SangKwon93 <apfhda7@gmail.com>
2022-10-02 21:58:45 +0900

    feat : 최고점수 추적하는 기능 추가

--- python20_21/main.py
+++ python20_21/main.py
@@ -25,21 +25,7 @@ screen.onkey(snake.right,"Right")
 
 
 import time
-# 방법1
-# tim=turtle.Turtle()
-# tim.shape("square")
-# tim.color("white")
-# tim.shapesize(1,3)
 
-snake_shape=[]
-# 방법2
-# starting_position=[(0,0),(-20,0),(-40,0)]
-# for position in starting_position:
-#     seq=Turtle("square")
-#     seq.color('white')
-#     seq.penup()
-#     seq.goto(position)
-#     snake_shape.append(seq)
 
 
 # 게임 작동하기
@@ -58,16 +44,21 @@ while is_game:
 
     # 벽에 충돌 감지 후 게임 종료
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        is_game =False # While문을 벗어난다.
-        scoreboard.game_over()
+        # is_game =False # While문을 벗어난다.
+        # scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+
 
     # 꼬리와 충돌 시 종료
-    for shape in snake.snake_shape[1:]: # 인덱싱을 활용한 꼬리 제외한 몸통에서 하나씩 for문
-        # if shape == snake.head:
-        #     pass
-        if snake.head.distance(shape)< 10:
-            is_game=False
-            scoreboard.game_over()
+    for shape in snake.snake_shape:
+        if shape == snake.head:
+            pass
+        elif snake.head.distance(shape)< 10:
+            # is_game=False
+            # scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 
